674f48d3ed61a3337277992d7380f741ee5e6a7a
Massimiliano Vella <massimilianovella01@gmail.com>
2024-05-08 11:02:24 +0200

    commit from Alex (he is having trouble with git). Adding playback functionalities as a core concept

---
+++ PlaybackFunction.py
@@ -0,0 +1,53 @@
+import MusicDatabase as mdb
+import pygame               #Library to play audio files (Da chönnt me oh eifch pygame.mixer bruche lol)
+from mutagen.mp3 import MP3 #Mutagen is a Python module to handle audio metadata --> This could be easier than letting the user input the metadata himself...
+from mutagen.wave import WAVE
+
+pygame.mixer.init()         #Initialize the mixer module
+#pygame.mixer.music.set_endevent(pygame. ?? ) #Trigger an event when the music ends (next song for example)
+
+current_position = 0 #Current position of the song in seconds --> For a progress bar/slider (let the progress bar change this value? Or Let  user Input change it?)
+paused = False     #Flag to check if the song is paused
+
+
+def play_song(song: mdb.Song):      #Frage: Wie willst Du den Song übergeben? Als Objekt?
+    if paused:
+        pygame.mixer.music.unpause()
+        paused = False
+    else:
+        pygame.mixer.music.load(song.path)
+        pygame.mixer.music.play()
+        paused = False              #zur Sicherheit
+
+def pause_song():       #Hier kann pausiert oder fortgesetzt werden
+    global paused
+    if paused:
+        pygame.mixer.music.unpause()
+        paused = False
+    else:
+        pygame.mixer.music.pause()
+        paused = True
+
+def stop_song():        
+    pygame.mixer.music.stop()
+    # pygame.mixer.music.unload() Hier vill. song rausnehmen?
+
+def song_queue(song: mdb.Song):
+    # Hier jeweils den nächsten Song in die Queue einfügen? Oder ist das obsolet
+
+# Position (in einer Progressbar) verändern - Achtung: geht nur für mp3
+def set_position(value : float):
+    global current_position
+    current_position = int(value) / 100 * pygame.mixer.music.get_length()
+    pygame.mixer.music.set_pos(current_position) #Hier die Progressbar rein? Oder die Current position und die Progressbar verändert die current position?
+
+def next_song():
+    pass
+
+def previous_song():
+    pass
+
+# To set the volume
+def volume(value : int):
+    volume = int(value) / 100               #pygame wants a value between 0 and 1 but the slider has a range from 0 to 100
+    pygame.mixer.music.set_volume(volume)
