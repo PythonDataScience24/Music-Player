5ecc7939ba028ec91a4356aa2817d840b606737b
Massimiliano Vella <massimilianovella01@gmail.com>
2024-05-10 14:08:41 +0200

    added the play functions to the song class so that the code is more modular

--- PlaybackFunction.py
+++ PlaybackFunction.py
@@ -1,79 +1,79 @@
-import MusicDatabase as mdb
-import sounddevice as sd
-import numpy as np
-from scipy.io.wavfile import read
+# import MusicDatabase as mdb
+# import sounddevice as sd
+# import numpy as np
+# from scipy.io.wavfile import read
 
-# Default sample rate for playback
-DEFAULT_SAMPLE_RATE = 44100
+# # Default sample rate for playback
+# DEFAULT_SAMPLE_RATE = 44100
 
-# Current position of the song in seconds
-current_position = 0
+# # Current position of the song in seconds
+# current_position = 0
 
-# Flag to check if the song is paused
-paused = False
+# # Flag to check if the song is paused
+# paused = False
 
-current_position = 0 #Current position of the song in seconds --> For a progress bar/slider (let the progress bar change this value? Or Let  user Input change it?)
-paused = False     #Flag to check if the song is paused
+# current_position = 0 #Current position of the song in seconds --> For a progress bar/slider (let the progress bar change this value? Or Let  user Input change it?)
+# paused = False     #Flag to check if the song is paused
 
 
-def play_song(song: mdb.Song):
-    global paused, current_position
-    #check if the state of paused is false if it is so then we play the song
-    if paused:
-        sd.play(song.audio_data, song.sample_rate, start=int(current_position * song.sample_rate))
-        paused = False
-    else:
-        # Read audio data from file and assign it to song.audio_data
-        # Discard the sample rate returned by read() using the underscore 
-        _, song.audio_data = read(song.file_path)
-        song.sample_rate = DEFAULT_SAMPLE_RATE  # Set default sample rate
-        sd.play(song.audio_data, song.sample_rate)
-        paused = False
-    print("Now playing:", song.title)
+# def play_song(song: mdb.Song):
+#     global paused, current_position
+#     #check if the state of paused is false if it is so then we play the song
+#     if paused:
+#         sd.play(song.audio_data, song.sample_rate, start=int(current_position * song.sample_rate))
+#         paused = False
+#     else:
+#         # Read audio data from file and assign it to song.audio_data
+#         # Discard the sample rate returned by read() using the underscore 
+#         _, song.audio_data = read(song.file_path)
+#         song.sample_rate = DEFAULT_SAMPLE_RATE  # Set default sample rate
+#         sd.play(song.audio_data, song.sample_rate)
+#         paused = False
+#     print("Now playing:", song.title)
 
-def pause_song():       #Hier kann pausiert oder fortgesetzt werden
-    global paused
-    if paused:
-        sd.resume()
-        paused = False
-    else:
-        sd.pause()
-        paused = True
+# def pause_song():       #Hier kann pausiert oder fortgesetzt werden
+#     global paused
+#     if paused:
+#         sd.resume()
+#         paused = False
+#     else:
+#         sd.pause()
+#         paused = True
 
-def stop_song():        
-    sd.stop()
+# def stop_song():        
+#     sd.stop()
 
-# def song_queue(song: mdb.Song):
-#     # Hier jeweils den nächsten Song in die Queue einfügen? Oder ist das obsolet
+# # def song_queue(song: mdb.Song):
+# #     # Hier jeweils den nächsten Song in die Queue einfügen? Oder ist das obsolet
 
-# Position (in einer Progressbar) verändern - Achtung: geht nur für mp3
-def set_position(song: mdb.Song, value : float):
-    global current_position
-    current_position = value
-    if not paused:
-        sd.stop()
-        play_song(song)
+# # Position (in einer Progressbar) verändern - Achtung: geht nur für mp3
+# def set_position(song: mdb.Song, value : float):
+#     global current_position
+#     current_position = value
+#     if not paused:
+#         sd.stop()
+#         play_song(song)
 
-def next_song():
-    pass
+# def next_song():
+#     pass
 
-def previous_song():
-    pass
+# def previous_song():
+#     pass
 
-# To set the volume
-def volume(song: mdb.Song, value : int):
-    # check the value if it is possible to amplify it that far 
-    #--> restriction so that the user cannot make misinputs --> ask the user how many percent they want to increase it
-    if value < 0 or value > 100:
-        raise ValueError("Volume value must be between 0 and 100.")
+# # To set the volume
+# def volume(song: mdb.Song, value : int):
+#     # check the value if it is possible to amplify it that far 
+#     #--> restriction so that the user cannot make misinputs --> ask the user how many percent they want to increase it
+#     if value < 0 or value > 100:
+#         raise ValueError("Volume value must be between 0 and 100.")
     
-    # Convert volume percentage to a scale factor
-    scale_factor = value / 100.0
+#     # Convert volume percentage to a scale factor
+#     scale_factor = value / 100.0
     
-    # Amplify the audio data using numpy
-    song.audio_data = np.multiply(song.audio_data, scale_factor)
+#     # Amplify the audio data using numpy
+#     song.audio_data = np.multiply(song.audio_data, scale_factor)
 
-    # If the song is currently playing, restart playback with the new volume
-    if not paused:
-        sd.stop()
-        play_song(song)
+#     # If the song is currently playing, restart playback with the new volume
+#     if not paused:
+#         sd.stop()
+#         play_song(song)
--- Song.py
+++ Song.py
@@ -14,7 +14,30 @@ Attributes:
 
 Methods:
     __init__: Initializes a Song object with the provided attributes.
+    play_song: play the chosen song
+    pause_song: pause the song and be able to play it again
+    stop_song: stop playing the song alltogether without being able to start it again
+    set_position: enable the user to play a song at a certain point
+    next_song: skip to the next song in a queue
+    previous_song: go back to the previous song played
+    volume: amplify the volume of the song playing
 '''
+
+import sounddevice as sd
+import numpy as np
+from scipy.io.wavfile import read
+
+# Default sample rate for playback
+DEFAULT_SAMPLE_RATE = 44100
+
+# Current position of the song in seconds
+current_position = 0
+
+# Flag to check if the song is paused
+paused = False
+
+current_position = 0 #Current position of the song in seconds --> For a progress bar/slider (let the progress bar change this value? Or Let  user Input change it?)
+paused = False     #Flag to check if the song is paused
 
 class Song:
     def __init__ (self, title, artist, genre, year, album, file_path, id = None):
@@ -25,4 +48,69 @@ class Song:
         self.album = album
         self.file_path = file_path
         self.id = id
-       
+
+
+    def play_song(self):
+        global paused, current_position
+        #check if the state of paused is false if it is so then we play the song
+        if paused:
+            sd.play(self.audio_data, self.sample_rate, start=int(current_position * self.sample_rate))
+            paused = False
+        else:
+        # Read audio data from file and assign it to song.audio_data
+        # Discard the sample rate returned by read() using the underscore 
+            _, self.audio_data = read(self.file_path)
+            self.sample_rate = DEFAULT_SAMPLE_RATE  # Set default sample rate
+            sd.play(self.audio_data, self.sample_rate)
+            paused = False
+        print("Now playing:", self.title)
+
+    def pause_song(self):       #Hier kann pausiert oder fortgesetzt werden
+        global paused
+        if paused:
+            sd.resume()
+            paused = False
+        else:
+            sd.pause()
+            paused = True
+
+    # here the song playing gets killed off
+    def stop_song(self):        
+        sd.stop()
+
+    # Position (in einer Progressbar) verändern - Achtung: geht nur für mp3
+    def set_position(self, value : float):
+        global current_position
+        current_position = value
+        if not paused:
+            sd.stop()
+            self.play_song()
+
+    
+    # def song_queue(song: mdb.Song):
+    # Hier jeweils den nächsten Song in die Queue einfügen? Oder ist das obsolet
+
+
+    def next_song():
+        pass
+
+    def previous_song():
+        pass
+
+    # To set the volume
+    def volume(self, value : int):
+        # check the value if it is possible to amplify it that far 
+        #--> restriction so that the user cannot make misinputs --> ask the user how many percent they want to increase it
+        if value < 0 or value > 100:
+            raise ValueError("Volume value must be between 0 and 100.")
+    
+        # Convert volume percentage to a scale factor
+        scale_factor = value / 100.0
+    
+        # Amplify the audio data using numpy
+        self.audio_data = np.multiply(self.audio_data, scale_factor)
+
+        #   If the song is currently playing, restart playback with the new volume
+        if not paused:
+            sd.stop()
+            self.play_song()
