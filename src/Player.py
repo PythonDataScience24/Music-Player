4e8f589fe559bd9c57ee882356e635284d4d3c20
UltraGitHub00 <julienchopin1@gmail.com>
2024-05-27 14:58:22 +0200

    docstring player.py

--- src/Player.py
+++ src/Player.py
@@ -28,7 +28,13 @@ import pygame
 
 
 class Player:
+    """
+    A music player class using pygame to manage song playback.
+    """
     def __init__(self):
+        """
+        Initializes the Player object and sets up the pygame mixer.
+        """
         pygame.init()
         pygame.mixer.init()
         self.current_song = None
@@ -38,6 +44,12 @@ class Player:
 
 
     def play_song(self, song):
+        """
+        Loads and plays the specified song.
+
+        Args:
+            song (Song): The song object to be played.
+        """
         self.offset = 0
         if self.current_song:
             pygame.mixer.music.stop()
@@ -47,21 +59,42 @@ class Player:
         self.song_length = pygame.mixer.Sound(song.file_path).get_length()
 
     def pause_song(self):
+        """
+        Pauses the currently playing song.
+        """
         pygame.mixer.music.pause()
 
     def resume_song(self):
+        """
+        Resumes playback of the paused song.
+        """
         pygame.mixer.music.unpause()
 
     def stop_song(self):
+        """
+        Stops the currently playing song.
+        """
         pygame.mixer.music.stop()
 
     def is_playing(self):
         return pygame.mixer.music.get_busy()
     
     def set_volume(self, volume):
+        """
+        Sets the playback volume.
+
+        Args:
+            volume (int): The volume level (0 to 100).
+        """
         pygame.mixer.music.set_volume(volume/100)
 
     def set_position(self, value):
+        """
+        Sets the playback position to a specific point.
+
+        Args:
+            value (float): The position to set in the song, as a percentage (0 to 100).
+        """
         if self.current_song:
             try:
                 self.offset = value - ((100/(self.song_length*1000)) * pygame.mixer.music.get_pos())
@@ -70,5 +103,12 @@ class Player:
                 pass
 
     def get_position(self):
+        """
+        Gets the current playback position as a percentage.
+
+        Returns:
+            float: The current playback position as a percentage of the total song length.
+        """
         if self.current_song:
             return ((100/(self.song_length*1000)) * pygame.mixer.music.get_pos()) + self.offset
+
